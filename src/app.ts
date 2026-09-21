7704007f15261f82fb20efb02b8e73d618a728c6
Horuiaf <s00272951@atu.ie>
2026-09-21 14:15:48 +0100

    task 3 done

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 4000;
 
 const app: Application = express();
 
+app.use((req, _res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+});
+
 app.get("/ping", async (_req: Request, res: Response) => {
     res.json({
         message: "hello from Himel"
