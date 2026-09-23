5396cf6e0160b1902d9d26ac27b3b7185bb4d016
Horuiaf Uzzaman <S00272951@atu.ie>
2026-09-23 10:04:38 +0100

    task 3 is done

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
-import express, {Application, Request, Response} from "express" ;
+import carRoutes from './routes/cars';
+import express, { Application, Request, Response } from "express";
 
 const PORT = process.env.PORT || 4000;
 
 const app: Application = express();
 
+app.use(express.json());
+
 app.use((req, _res, next) => {
     console.log(`${req.method} ${req.originalUrl}`);
     next();
 });
+
+app.use('/api/v1/cars', carRoutes);
 
 app.get("/ping", async (_req: Request, res: Response) => {
     res.json({
@@ -15,12 +20,12 @@ app.get("/ping", async (_req: Request, res: Response) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
-});
-
 app.get('/bananas', async (_req: Request, res: Response) => {
     res.json({
         message: "this is bananas",
     });
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
 });
