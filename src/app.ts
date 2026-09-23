e48d10747772d24b30c9ff12f2a2a7867dc423aa
Horuiaf Uzzaman <S00272951@atu.ie>
2026-09-23 11:06:48 +0100

    mongodb url has been added to .env

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import { env } from "./config/env";
 import carRoutes from './routes/cars';
 import express, { Application, Request, Response } from "express";
+import { env } from "./config/env";
+import { connectDB } from "./config/database";
 
-const PORT = env.port;
+const port = env.port;
 
 const app: Application = express();
 
@@ -27,6 +28,12 @@ app.get('/bananas', async (_req: Request, res: Response) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
-});
+const startServer = async () => {
+    await connectDB();
+
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+};
+
+startServer();
